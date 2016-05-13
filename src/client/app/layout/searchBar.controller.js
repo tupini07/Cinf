ce66d041fb311b32f5c9050879381dc9f4074ff2
andre tupini <andre tupini>
2016-05-13 03:14:37 -0600

    search button is only visible in search state

--- src/client/app/layout/searchBar.controller.js
+++ src/client/app/layout/searchBar.controller.js
@@ -5,23 +5,34 @@
         .module('app.layout')
         .controller('SearchBarController', SearchBarController);
 
-    SearchBarController.$inject = ['$rootScope', 'cinemaService', 'movieService']
-
-    function SearchBarController($rootScope, cinemaService, movieService) {
+    SearchBarController.$inject = ['$rootScope', '$state', '$scope', 'cinemaService', 'movieService']
+    function SearchBarController($rootScope, $state, $scope, cinemaService, movieService) {
         var vm = this;
         vm.date = new Date();
         vm.cinemas = cinemaService.getCinemas();
         vm.doSearch = doSearch;
+        vm.currentState = $state.current.name;
 
         // TODO Pointless... For the moment we will leave it but it makes no sense to display the movies
         // in the searchbar and in the search content.\\\
         vm.movies = movieService.getMovies(vm.date, vm.cinemas);
 
+
         activate();
 
-        function activate() {}
+        function activate() {
+            var stateChangedLstner = $scope.$on('$stateChangeSuccess', function(subsData, newState) {
+              vm.currentState =  newState.name;
+            });
+
+            //unsubscribe from event
+            $scope.$on('$destroy', function() {
+                stateChangedLstner();
+            });
+        }
 
         function doSearch() {
+          
             //Starts a search on the moviesearch service and broadcasts it's promise which is caught
             //by the 'searchContController'
             $rootScope.$broadcast('searchRequested', movieService.getMoviesPromise());
