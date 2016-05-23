c88a46100675bbe40932cbcecb96c021eb595321
Andrea Tupini <tupini07@gmail.com>
2016-05-23 17:51:44 -0600

    comment on broadcast, searchBarCntrl

--- src/client/app/layout/searchBar.controller.js
+++ src/client/app/layout/searchBar.controller.js
@@ -42,6 +42,13 @@
 
       //Starts a search on the moviesearch service and broadcasts it's promise which is caught
       //by the 'searchContController'
+      /*****?
+       This could be moved to the movieService so that the controller complies with the separation of concern principle but
+       that in turn would mean that the service is responsable for broadcasting the message, which is not since this is just
+       a special case between this controller and the movieSearch controller.
+       In other words, it's the concern of this
+       controller that the movieSearch controller gets informed of the search beign made.
+      ******/
       $rootScope.$broadcast('searchRequested', movieService.searchMovies(searchParams));
     }
   }
